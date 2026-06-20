14eb6ce8992bd57d1eaf1c25f3620c6881733311
Alejandro Diaz Limon <62073381+staticvoidmainmaui@users.noreply.github.com>
2026-06-19 20:49:48 -0400

    Time: 0 ms (100%), Space: 50.2 MB (32.79%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int m=0;
+        int count=0;
+        for(int i=0 ; i< nums.size(); i++){
+            if(nums[i]==1){
+                count++;
+            }else{
+                count=0;
+            }
+            
+            if(count>m){
+                m=count;
+            }
+        
+            
+        }
+        return m;
+    }
+};
