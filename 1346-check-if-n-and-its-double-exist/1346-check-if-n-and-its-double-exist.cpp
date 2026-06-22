178ef4b7ea36490322e6ee9dee5d92313ef5e2ef
Alejandro Diaz Limon <62073381+staticvoidmainmaui@users.noreply.github.com>
2026-06-22 18:43:25 -0400

    Time: 0 ms (100%), Space: 15 MB (8.98%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -6,7 +6,7 @@ public:
         for(int i =0; i<arr.size();i++){
             if(hashset.count((double) 2*arr[i])){
                 return true;
-            } else if(hashset.count(((double) arr[i])/2)){
+            } else if(hashset.count((double)arr[i]/2)){
                 return true;
             }else{
                 hashset.insert((double) arr[i]);
