6203d2d734f40106f06a6488ab0295844cb5c00b
Alejandro Diaz Limon <62073381+staticvoidmainmaui@users.noreply.github.com>
2026-06-22 18:24:36 -0400

    Time: 3 ms (26.71%), Space: 15.1 MB (8.98%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<double> hashset;
+
+        for(int i =0; i<arr.size();i++){
+            if(hashset.count((double) 2*arr[i])){
+                return true;
+            } else if(hashset.count(((double) arr[i])/2)){
+                return true;
+            }else{
+                hashset.insert((double) arr[i]);
+                //hashset.insert(2*arr[i]);
+            }
+        }
+        return false;
+    }
+};
