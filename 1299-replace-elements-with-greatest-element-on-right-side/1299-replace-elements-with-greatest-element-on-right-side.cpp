5cde059796b7392d621d7b7c6947f715047b4dec
Alejandro Diaz Limon <62073381+staticvoidmainmaui@users.noreply.github.com>
2026-06-24 01:56:32 -0400

    Time: 3 ms (46.05%), Space: 72.6 MB (95.57%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,25 +2,39 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         // [17,18,5,4,6,1]
-        vector<int> copy=arr;
+        // vector<int> copy=arr;
         
 
-        int biggest=arr.size()-1;
-        for(int i=arr.size()-2; i>=0;i--){  
+        // int biggest=arr.size()-1;
+        // for(int i=arr.size()-2; i>=0;i--){  
             
-            if(copy[i+1]<=copy[biggest]) {  
-                arr[i]=copy[biggest]; 
-            } else if(copy[i+1]>copy[biggest]){
-                biggest=i+1; 
-                arr[i]=copy[biggest];
-            }
-            // if(arr[i-1]>=copy[k]){
-            //     k=i;
-            // } else{
-            //     arr[i-1]==copy[k];
-            // }
+        //     if(copy[i+1]<=copy[biggest]) {  
+        //         arr[i]=copy[biggest]; 
+        //     } else if(copy[i+1]>copy[biggest]){
+        //         biggest=i+1; 
+        //         arr[i]=copy[biggest];
+        //     }
+        //     // if(arr[i-1]>=copy[k]){
+        //     //     k=i;
+        //     // } else{
+        //     //     arr[i-1]==copy[k];
+        //     // }
+        // }
+        // arr[arr.size()-1]=-1;
+        // return arr;
+
+        //different solution
+        int maxRight=-1;
+        for(int i= arr.size()-1;i>=0;--i){
+            int newMax= max(maxRight, arr[i]);
+            //int cur=arr[i]; not even needed as max val of integers considered would be in newMax
+            arr[i]=maxRight;
+            maxRight= newMax;
         }
-        arr[arr.size()-1]=-1;
+
         return arr;
+
+
+
     }
 };
