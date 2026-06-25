89141196b11f7b5414cb37a888b4c2a6474efb21
Alejandro Diaz Limon <62073381+staticvoidmainmaui@users.noreply.github.com>
2026-06-25 00:30:41 -0400

    Time: 0 ms (100%), Space: 20.7 MB (51.82%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -4,7 +4,7 @@ public:
         int k =0;
         int temp=0;
         for(int i=0; i <=nums.size()-1 ;i++){
-            if(nums[i]%2==0){
+            if(nums[i]%2==0){  //if (val%2) does opposite as it evaluates true for odd
                 temp=nums[k];
                 nums[k]=nums[i];
                 nums[i]=temp;
